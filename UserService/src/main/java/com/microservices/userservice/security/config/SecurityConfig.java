cba2c55b2717fa5f27cd99f48cda56fc5affd7f9
Jorge Lopez Valdez <jorge.lopezvz@udlap.mx>
2024-05-03 16:41:13 -0600

    MSP-013: Creating Prometheus and Grafana setup

--- UserService/src/main/java/com/microservices/userservice/security/config/SecurityConfig.java
+++ UserService/src/main/java/com/microservices/userservice/security/config/SecurityConfig.java
@@ -3,16 +3,16 @@ package com.microservices.userservice.security.config;
 import com.microservices.userservice.security.filter.JwtTokenVerifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity(prePostEnabled = true)
+@Configuration
 public class SecurityConfig {
 
     private final JwtTokenVerifier jwtTokenVerifier;
--- UserService/src/test/java/com/microservices/userservice/UserServiceApplicationTests.java
+++ UserService/src/test/java/com/microservices/userservice/UserServiceApplicationTests.java
@@ -1,10 +1,11 @@
 package com.microservices.userservice;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UserserviceApplicationTests {
+class UserServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
